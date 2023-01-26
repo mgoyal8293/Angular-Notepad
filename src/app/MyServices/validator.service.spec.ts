ad6f6f787760c751c20f8d99800d6c6b97d9a11b
Munish Goyal <munish.goyal2010@gmail.com>
2023-01-26 15:05:40 +0530

    Added Validations for empty fields.

--- src/app/MyServices/validator.service.spec.ts
+++ src/app/MyServices/validator.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { LoginService } from './login.service';
+import { ValidatorService } from './validator.service';
 
-describe('LoginService', () => {
-  let service: LoginService;
+describe('ValidatorService', () => {
+  let service: ValidatorService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(LoginService);
+    service = TestBed.inject(ValidatorService);
   });
 
   it('should be created', () => {
