22621b2f7a037c9e73356baac199a569f7ad825c
Munish Goyal <munish.goyal2010@gmail.com>
2023-01-27 03:33:38 +0530

    Made login validations responsive.

--- src/app/MyComponents/login/login.component.ts
+++ src/app/MyComponents/login/login.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HelperService } from 'src/app/MyServices/helper.service';
 import { HttpService } from 'src/app/MyServices/http.service';
@@ -13,38 +14,43 @@ import { CONSTANTS } from '../notepad-static-data';
 })
 export class LoginComponent {
 
-  public userName: string = '';
-  public password: string = '';
+  public newUser: boolean = false;
   public appLoading: boolean = false;
+  public formSignup: FormGroup;
 
   private _validator = new ValidatorService();
   private _helper = new HelperService();
   private _httpHelper = new HttpService(this._httpClient);
 
-  constructor(private router: Router, private _httpClient: HttpClient) { }
+  constructor(private router: Router, private _httpClient: HttpClient, private builder: FormBuilder) {
+    this.formSignup = this._validator.createLogInForm(builder, this.newUser);
+  }
 
-  clickLoginButton(isSigningUp?: boolean) {
-    let _validation = this._validator.loginDetails(this.userName, this.password);
+  getInput(key: string) {
+    return this.formSignup.controls[key];
+  }
 
-    if (_validation.error) {
-      alert(_validation.message);
-      return;
-    }
+  clickLoginButton() {
     this.appLoading = true;
 
-    isSigningUp ? this._httpHelper.getDataFromDB().subscribe(_response => this._isUserAlreadyPresent(_response)) :
+    this.newUser ? this._httpHelper.getDataFromDB().subscribe(_response => this._isUserAlreadyPresent(_response)) :
       this._httpHelper.getDataFromDB().subscribe(_response => this._afterRecievingdata(_response));
   }
 
+  clickSignUpButton(_goBack: boolean) {
+    this.newUser = !_goBack;
+    this.formSignup = this._validator.createLogInForm(this.builder, this.newUser);
+  }
+
   _isUserAlreadyPresent(_response: any) {
-    let authUser: any = this._helper.getAuthUserData(_response, this.userName, this.password);
+    let authUser: any = this._helper.getAuthUserData(_response, this.getInput('email').value, this.getInput('password').value);
     if (authUser._id) {
       //error
       this.appLoading = false;
-      alert('Username already present');
+      alert('Email with "' + this.getInput('email').value + '" name already exists. Please Try Login');
       window.location.reload();
     } else {
-      this._httpHelper.addNewUserToDB(this.userName, this.password).subscribe(_response => this._afterAddingNewUser(_response));
+      this._httpHelper.addNewUserToDB(this.getInput('email').value, this.getInput('password').value).subscribe(_response => this._afterAddingNewUser(_response));
     }
   }
 
@@ -54,13 +60,13 @@ export class LoginComponent {
   }
 
   _afterRecievingdata(_response: any) {
-    let authUser: any = this._helper.getAuthUserData(_response, this.userName, this.password);
+    let authUser: any = this._helper.getAuthUserData(_response, this.getInput('email').value, this.getInput('password').value);
     if (!authUser.error) {
       this._helper.saveToLocalStorage(authUser);
       this.router.navigate(['/' + CONSTANTS.homePageRoute, authUser._id]);
       return;
     }
-    alert('Username not found');
+    alert('No Email with "' + this.getInput('email').value + '" name exists. Please Try Signup');
     window.location.reload();
   }
 }
